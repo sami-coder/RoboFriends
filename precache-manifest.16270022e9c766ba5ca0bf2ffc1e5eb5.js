7de7393f01ad3189460b2406988649e8bd883748
sumit <smitkawale@gmail.com>
2020-08-22 11:23:32 -0400

    Updates

---
+++ precache-manifest.16270022e9c766ba5ca0bf2ffc1e5eb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b490320b2f76c3edf6825f70cc35df5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "18d48b667bcca0ecdbb3",
+    "url": "/static/css/main.a8e8fc53.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d48b667bcca0ecdbb3",
+    "url": "/static/js/main.c2b444b9.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
